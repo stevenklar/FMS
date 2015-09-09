a245a1962971ce653326a50a88bed4fd6921c0e9
Steven Klar <stevenklar@Stevens-MacBook-Pro.local>
2015-09-10 00:54:38 +0200

    Fix ressource path

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,10 @@
 
     <title>FMS</title>
 
-    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="./bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./dist/css/sb-admin-2.css" rel="stylesheet">
 
-    <script src="/bower_components/angular/angular.js"></script>
+    <script src="./bower_components/angular/angular.js"></script>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
